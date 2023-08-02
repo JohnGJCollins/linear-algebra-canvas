e96e8abb56b303be2102b23d90fa6e636e5e2ff6
John Collins <johngjcollins@gmail.com>
2023-08-02 16:33:20 -0400

    Created main files and added basic code.

---
+++ main.js
@@ -0,0 +1,7 @@
+let plane = document.getElementById("plane");
+let context = plane.getContext("2d");
+
+if(canvas.fill) {
+    context.fillStyle = "rgb(100, 0, 255)";
+    context.fillRect(0, 0, 100, 100);
+}
