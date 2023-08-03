80e02b878a5950fb1cf1e815ee6e435eca62bf9e
John Collins <johngjcollins@gmail.com>
2023-08-02 21:03:09 -0400

    Added panning and axes to render mode

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let m12 = document.getElementById("m12");
 let m21 = document.getElementById("m21");
 let m22 = document.getElementById("m22");
 let renderMode = document.getElementById("render-mode");
-let clearButton = document.getElementById("clear");
+let clearButton = document.getElementById("clear-points");
 let plane = document.getElementById("plane");
 let context = plane.getContext("2d");
 
@@ -11,36 +11,61 @@ let pointRadius = 5;
 let pointColor = "rgb(100, 0, 255)";
 let lineWidth = 2;
 let lineColor = "rgba(0, 255, 0, 128)";
+let arrowRadius = 5;
 
 let points = [];
 let centerPosition = {x: 0, y: 0};
+let pan = false;
+let panLast = null;
 
 if(context.fill) {
     render();
 
     plane.onmousedown = function(e) {
-        if(renderMode.value !== "edit") {
-            // TODO: add click-drag panning
-
-            render();
-            return;
-        }
-
         let rectangle = e.target.getBoundingClientRect();
         let x = e.clientX - rectangle.left - plane.width / 2;
         let y = e.clientY - rectangle.top - plane.height / 2;
 
+        if(renderMode.value !== "edit") {
+            // TODO: add click-drag panning
+            pan = true;
+            panLast = {x: x, y: y};
+            render();
+            return;
+        }
+
         points.push({x: x, y: y});
         render();
+    };
+
+    window.onmouseup = function(e) {
+        pan = false;
+    };
+
+    plane.onmousemove = function(e) {
+        if(pan) {
+            let rectangle = e.target.getBoundingClientRect();
+            let x = e.clientX - rectangle.left - plane.width / 2;
+            let y = e.clientY - rectangle.top - plane.height / 2;
+
+            centerPosition.x -= x - panLast.x;
+            centerPosition.y -= y - panLast.y;
+            panLast = {x: x, y: y};
+            render();
+        }
     };
 
     m11.addEventListener("input", render);
     m12.addEventListener("input", render);
     m21.addEventListener("input", render);
     m22.addEventListener("input", render);
-    renderMode.addEventListener("input", render);
+    renderMode.addEventListener("input", () => {
+        centerPosition = {x: 0, y: 0};
+        render();
+    });
     clearButton.addEventListener("click", () => {
         points = [];
+        centerPosition = {x: 0, y: 0};
         render();
     });
 }
@@ -71,23 +96,149 @@ function render() {
 }
 
 function drawAxes() {
-    if(renderMode.value !== "edit") {
-        // TODO: axes should be transformed when in render mode
-        return;
+    let axisHeight = plane.height / 2 - centerPosition.y;
+    let axisDisplacement = plane.width / 2 - centerPosition.x;
+
+    if(axisHeight < 0) {
+        axisHeight = 1;
+    } else if(axisHeight > plane.height) {
+        axisHeight = plane.height - 1;
     }
 
-    context.lineWidth = 1;
+    if(axisDisplacement < 0) {
+        axisDisplacement = 1;
+    } else if(axisDisplacement > plane.width) {
+        axisDisplacement = plane.width - 1;
+    }
+
     context.strokeStyle = "rgb(0, 0, 0)";
+    context.lineWidth = 1;
 
+    // x-axis line
     context.beginPath();
-    context.moveTo(plane.width / 2, 0);
-    context.lineTo(plane.width / 2, plane.height);
+    context.moveTo(0, axisHeight);
+    context.lineTo(plane.width, axisHeight);
+    context.stroke();
+
+    // y-axis line
+    context.beginPath();
+    context.moveTo(axisDisplacement, 0);
+    context.lineTo(axisDisplacement, plane.height);
+    context.stroke();
+
+    context.lineWidth = 2;
+
+    // left arrow
+    context.beginPath();
+    context.moveTo(1, axisHeight);
+    context.lineTo(1 + arrowRadius, axisHeight - arrowRadius);
     context.stroke();
 
     context.beginPath();
-    context.moveTo(0, plane.height / 2);
-    context.lineTo(plane.width, plane.height / 2);
+    context.moveTo(1, axisHeight);
+    context.lineTo(1 + arrowRadius, axisHeight + arrowRadius);
     context.stroke();
+
+    // right arrow
+    context.beginPath();
+    context.moveTo(plane.width - 1, axisHeight);
+    context.lineTo(plane.width - 1 - arrowRadius, axisHeight - arrowRadius);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(plane.width - 1, axisHeight);
+    context.lineTo(plane.width - 1 - arrowRadius, axisHeight + arrowRadius);
+    context.stroke();
+
+    // top arrow
+    context.beginPath();
+    context.moveTo(axisDisplacement, 1);
+    context.lineTo(axisDisplacement - arrowRadius, 1 + arrowRadius);
+    context.stroke();
+    
+    context.beginPath();
+    context.moveTo(axisDisplacement, 1);
+    context.lineTo(axisDisplacement + arrowRadius, 1 + arrowRadius);
+    context.stroke();
+
+    // bottom arrow
+    context.beginPath();
+    context.moveTo(axisDisplacement, plane.height - 1);
+    context.lineTo(axisDisplacement - arrowRadius, plane.height - 1 - arrowRadius);
+    context.stroke();
+    
+    context.beginPath();
+    context.moveTo(axisDisplacement, plane.height - 1);
+    context.lineTo(axisDisplacement + arrowRadius, plane.height - 1 - arrowRadius);
+    context.stroke();
+
+        // if(centerPosition.y + plane.height / 2 < 0) {
+
+        // } else if(centerPosition.y - plane.height / 2 > plane.height) {
+            
+        // } else {
+        //     // x-axis line
+        //     context.beginPath();
+        //     context.moveTo(0, plane.height / 2 - centerPosition.y);
+        //     context.lineTo(plane.width, plane.height / 2 - centerPosition.y);
+        //     context.stroke();
+
+        //     // left arrow
+        //     context.beginPath();
+        //     context.moveTo(0, plane.height / 2 - centerPosition.y);
+        //     context.lineTo(arrowRadius, plane.height / 2 - centerPosition.y - arrowRadius);
+        //     context.stroke();
+
+        //     context.beginPath();
+        //     context.moveTo(0, plane.height / 2 - centerPosition.y);
+        //     context.lineTo(arrowRadius, plane.height / 2 - centerPosition.y + arrowRadius);
+        //     context.stroke();
+
+        //     // right arrow
+        //     context.beginPath();
+        //     context.moveTo(plane.width, plane.height / 2 - centerPosition.y);
+        //     context.lineTo(plane.width - arrowRadius, plane.height / 2 - centerPosition.y - arrowRadius);
+        //     context.stroke();
+
+        //     context.beginPath();
+        //     context.moveTo(plane.width, plane.height / 2 - centerPosition.y);
+        //     context.lineTo(plane.width - arrowRadius, plane.height / 2 - centerPosition.y + arrowRadius);
+        //     context.stroke();
+        // }
+
+        // if(centerPosition.x + plane.width / 2 < 0) {
+
+        // } else if(centerPosition.x - plane.width / 2 > plane.width) {
+            
+        // } else {
+        //     // y-axis line
+        //     context.beginPath();
+        //     context.moveTo(plane.width / 2 - centerPosition.x, 0);
+        //     context.lineTo(plane.width / 2 - centerPosition.x, plane.height);
+        //     context.stroke();
+
+        //     // top arrow
+        //     context.beginPath();
+        //     context.moveTo(plane.width / 2 - centerPosition.x, 0);
+        //     context.lineTo(plane.width / 2 - centerPosition.x - arrowRadius, arrowRadius);
+        //     context.stroke();
+
+        //     context.beginPath();
+        //     context.moveTo(plane.width / 2 - centerPosition.x, 0);
+        //     context.lineTo(plane.width / 2 - centerPosition.x + arrowRadius, arrowRadius);
+        //     context.stroke();
+
+        //     // bottom arrow
+        //     context.beginPath();
+        //     context.moveTo(plane.width / 2 - centerPosition.x, plane.height);
+        //     context.lineTo(plane.width / 2 - centerPosition.x - arrowRadius, plane.height - arrowRadius);
+        //     context.stroke();
+
+        //     context.beginPath();
+        //     context.moveTo(plane.width / 2 - centerPosition.x, plane.height);
+        //     context.lineTo(plane.width / 2 - centerPosition.x + arrowRadius, plane.height - arrowRadius);
+        //     context.stroke();
+        // }
 }
 
 function drawPoint(point) {
@@ -107,5 +258,5 @@ function transformPoint(point) {
     }
 
     let matrix = [[m11.value || 0, m12.value || 0], [m21.value || 0, m22.value || 0]];
-    return {x: point.x * matrix[0][0] + point.y * matrix[0][1], y: point.x * matrix[1][0] + point.y * matrix[1][1]};
+    return {x: point.x * matrix[0][0] + point.y * matrix[0][1] - centerPosition.x, y: point.x * matrix[1][0] + point.y * matrix[1][1] - centerPosition.y};
 }
